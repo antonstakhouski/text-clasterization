ed0b1b6b01e3a02fd4b330625a18e07fd207f3d8
Anton Stakhouski <antonstakhouski@gmail.com>
2017-04-13 06:51:06 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-#  import re
 
 
 def gloss_open(name):
@@ -10,46 +9,71 @@ def gloss_open(name):
     return dct
 
 
+def gloss_init(glossaries):
+    for dcp in discps:
+        glossaries.append([gloss_open(dcp), dcp])
+    return glossaries
+
+
+def clear_glossies(glossaries):
+    for gloss in glossaries:
+        gloss[0] = {a: 0 for a in gloss[0]}
+    return glossaries
+
+
+def slice_find(string, substring):
+    pos = string.find(substring)
+    if pos >= 0:
+        return string[:pos]
+    else:
+        return string
+
+
+def print_gloss(glossaries):
+    for gloss in glossaries:
+        print("-----" + gloss[1] + "------")
+        for el in gloss[0]:
+            if gloss[0][el] > 0:
+                print(el)
+
+
+def count_terms(text, glossaries, vec):
+    term_sum = 0
+    for line in text:
+        word = line
+        word = slice_find(word, "|")
+        word = slice_find(word, "?")
+        for gloss in glossaries:
+            if word in gloss[0]:
+                term_sum += 1
+                gloss[0][word] += 1
+                vec[gloss[1]] += 1
+    return term_sum
+
+
+def get_vectors(discps, glossaries):
+    vecs = list()
+    for discp in discps:
+        print(discp)
+        for num in range(1, 6):
+            vec = {a: 0 for a in discps}
+            glossaries = clear_glossies(glossaries)
+            path = "txt/" + discp + "/" + str(num)
+            lemmas = os.popen("./mystem -ln " + path).read().lower().split()
+            res = count_terms(lemmas, glossaries, vec)
+            term_sum = res
+            #  print_gloss(glossaries)
+            for gloss in glossaries:
+                vec[gloss[1]] = vec[gloss[1]] / term_sum
+            vecs.append([discp, num, vec])
+            print(num, vec)
+    return vecs
+
+
 discps = ("econ", "phyl", "med")
 
 glossaries = list()
-for dcp in discps:
-    glossaries.append([gloss_open(dcp), dcp])
-#  print(glossaries[0][0])
+glossaries = gloss_init(glossaries)
 
-#  for gloss in glossaries:
-#      for term in gloss[0]:
-#          vec[gloss[1]] += len([m.start() for m in re.finditer(term, txt)])
-
-for discp in discps:
-    print(discp)
-    for num in range(1, 6):
-        vec = {a: 0 for a in discps}
-        for gloss in glossaries:
-            gloss[0] = {a: 0 for a in gloss[0]}
-        path = "txt/" + discp + "/" + str(num)
-        res = os.popen("./mystem -ln " + path).read()
-        res = res.lower().split()
-        #  print(res)
-        for line in res:
-            word = line
-            pos = word.find("|")
-            if pos >= 0:
-                word = word[:pos]
-            pos = word.find("?")
-            if pos >= 0:
-                word = word[:pos]
-            #  print(word)
-            for word in line.split():
-                #  print(word)
-                for gloss in glossaries:
-                    if word in gloss[0]:
-                        gloss[0][word] += 1
-                        vec[gloss[1]] += 1
-        #  for gloss in glossaries:
-        #      print("-----" + gloss[1] + "------")
-        #      for el in gloss[0]:
-        #          #  print(gloss[0])
-        #          if gloss[0][el] > 0:
-        #              print(el)
-        print(num, vec)
+vecs = list()
+vecs = get_vectors(discps, glossaries)
