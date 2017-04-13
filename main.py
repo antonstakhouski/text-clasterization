3198d22d5f133cbfe61e05e0ff087c9c1c319b2e
Anton Stakhouski <antonstakhouski@gmail.com>
2017-04-13 09:16:52 +0300

    base vectors added

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 import os
+import math
+
+
+def sqr(x):
+    return x * x
 
 
 def gloss_open(name):
@@ -51,11 +56,12 @@ def count_terms(text, glossaries, vec):
     return term_sum
 
 
-def get_vectors(discps, glossaries):
+def get_vectors(discps, glossaries, txt_in_sect):
+    #  vecs = list()
     vecs = list()
     for discp in discps:
         print(discp)
-        for num in range(1, 6):
+        for num in range(1, txt_in_sect + 1):
             vec = {a: 0 for a in discps}
             glossaries = clear_glossies(glossaries)
             path = "txt/" + discp + "/" + str(num)
@@ -65,15 +71,55 @@ def get_vectors(discps, glossaries):
             #  print_gloss(glossaries)
             for gloss in glossaries:
                 vec[gloss[1]] = vec[gloss[1]] / term_sum
-            vecs.append([discp, num, vec])
+            vecs.append(vec)
             print(num, vec)
     return vecs
 
 
 discps = ("econ", "phyl", "med")
 
+txt_in_sect = 2
 glossaries = list()
 glossaries = gloss_init(glossaries)
 
-vecs = list()
-vecs = get_vectors(discps, glossaries)
+vecs = get_vectors(discps, glossaries, txt_in_sect)
+clusters = {a: list() for a in discps}
+#  print(vecs)
+
+max_disc_vec = {a: vecs[0] for a in discps}
+for vec in vecs:
+    for discp in discps:
+        if vec[discp] > max_disc_vec[discp][discp]:
+            max_disc_vec[discp] = vec
+for discp in discps:
+    clusters[discp].extend([max_disc_vec[discp], [max_disc_vec[discp], ]])
+
+for vec in vecs:
+    for discp in discps:
+        if vec in clusters[discp][1]:
+            vecs.remove(vec)
+
+#  for vec in vecs:
+#      best_cluster = discps[0]
+#      min_len = 1
+#      for discp in discps:
+#          sum_d = 0
+#          for d in discps:
+#              sum_d += sqr(clusters[discp][1] - vec[d])
+#          leng = math.sqrt(sum_d)
+#          if leng < min_len:
+#              min_len = leng
+#              best_cluster = d
+
+    #  clusters[best_cluster][0].append(vec)
+    #  center = clusters[best_cluster][1]
+    #  for el in clusters[best_cluster][0]:
+    #      sum_f = 0
+    #      for d in discps:
+    #          sum_fclusters[best_cluster][0][d]
+
+    #  vecs.remove(vec)
+
+
+print(clusters)
+print(len(vecs))
