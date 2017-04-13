09bd23d1cf1a5e15a7e0a487da4a4932806a52c3
Anton Stakhouski <antonstakhouski@gmail.com>
2017-04-13 05:34:52 +0300

    precision greatly improved

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 #  import re
 
 
@@ -13,9 +14,7 @@ discps = ("econ", "phyl", "med")
 
 glossaries = list()
 for dcp in discps:
-    glossaries.append((gloss_open(dcp), dcp))
-vec = {a: 0 for a in discps}
-
+    glossaries.append([gloss_open(dcp), dcp])
 #  print(glossaries[0][0])
 
 #  for gloss in glossaries:
@@ -25,13 +24,32 @@ vec = {a: 0 for a in discps}
 for discp in discps:
     print(discp)
     for num in range(1, 6):
-        text = open("txt/" + discp + "/" + str(num), "r")
-        txt = text.read().lower().split("\n")
-        for line in txt:
+        vec = {a: 0 for a in discps}
+        for gloss in glossaries:
+            gloss[0] = {a: 0 for a in gloss[0]}
+        path = "txt/" + discp + "/" + str(num)
+        res = os.popen("./mystem -ln " + path).read()
+        res = res.lower().split()
+        #  print(res)
+        for line in res:
+            word = line
+            pos = word.find("|")
+            if pos >= 0:
+                word = word[:pos]
+            pos = word.find("?")
+            if pos >= 0:
+                word = word[:pos]
+            #  print(word)
             for word in line.split():
                 #  print(word)
                 for gloss in glossaries:
                     if word in gloss[0]:
+                        gloss[0][word] += 1
                         vec[gloss[1]] += 1
-        text.close()
+        #  for gloss in glossaries:
+        #      print("-----" + gloss[1] + "------")
+        #      for el in gloss[0]:
+        #          #  print(gloss[0])
+        #          if gloss[0][el] > 0:
+        #              print(el)
         print(num, vec)
